af7728d71ef3b736cd94a077c65ca148486404d0
Sandman2020 <rsand20@wsr.k12.ia.us>
2019-09-14 11:52:31 -0500

    Webcam code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Mech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Mech.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robot_Parts.*;
