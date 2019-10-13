d0899555d4fe3a0aa1b105674a3e6d71ce4b94d7
Mason E <mellerbroek21@wsr.k12.ia.us>
2019-10-13 14:21:39 -0500

    purging all unneeded code for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parts/Controllers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parts/Controllers.java
@@ -5,9 +5,7 @@ public class Controllers extends LinearOpMode {
     // Initialize variables
     Controllers controller;
     int armRaiseSpeed = 5;
-    Arm arm;
-    Claw claw;
-    Wheel_Intake Wheel_Intake;
+
 
     public Controllers() {
     }
@@ -46,33 +44,13 @@ public class Controllers extends LinearOpMode {
     }
 
     public void BackBumpers() {
-        if (gamepad1.dpad_down) {
-            claw.SetClawPosition(100);
-        } else if (gamepad1.dpad_up) {
-            claw.SetClawPosition(0);
-        }
     }
 
     public void AButton() {
-        if (gamepad1.a) {
-            if (Wheel_Intake.currentState == "On" || gamepad1.timestamp > (Wheel_Intake.lastChange)) {
-                Wheel_Intake.currentState = "False";
-                Wheel_Intake.wheelSetPower(0);
-                Wheel_Intake.lastChange = gamepad1.timestamp;
-            } else {
-                Wheel_Intake.currentState = "On";
-                Wheel_Intake.wheelSetPower(1);
-                Wheel_Intake.lastChange = gamepad1.timestamp;
-            }
-        }
 
     }
 
     public void BackTriggers() {
-        if (gamepad2.left_bumper) {
-            arm.SetPosition(arm.armPositionStorage + controller.armRaiseSpeed);
-        } else if (gamepad2.right_bumper) {
-            arm.SetPosition(controller.armRaiseSpeed * -1 + arm.armPositionStorage);
-        }
+
     }
 }
